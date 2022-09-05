849ba06cfda0f37ef4b2fa2db24f9a59657eb679
Pranjal Singh <89650180+drigas7@users.noreply.github.com>
2022-09-05 18:10:23 +0530

    Update main.c

--- codes/main.c
+++ codes/main.c
@@ -7,16 +7,16 @@
 int main (void)
 {bool P,Q,R,F;
 DDRD = 0b00000000;
-PORTD= 0b00111000;  // digital pins 3,4,5 as input
+PORTD= 0b00011100;  // digital pins 3,4,5 as input
 DDRB    |= ((1 << DDB5));  // digital pin 13 led as output
 
 while(1)
 {
 	
 
-    P = (PIND & (1<<PD3)) ? 1:0;
-    Q = (PIND & (1<<PD4)) ? 1:0;
-    R = (PIND & (1<<PD5)) ? 1:0;
+    P = (PIND & (1<<PD2)) ? 1:0;
+    Q = (PIND & (1<<PD3)) ? 1:0;
+    R = (PIND & (1<<PD4)) ? 1:0;
     F = (P||Q||R)&(P||!Q||!R)&(!P||Q||R)&(!P||Q||!R);
     F=!F;
 	PORTB |=(F<<5);	
